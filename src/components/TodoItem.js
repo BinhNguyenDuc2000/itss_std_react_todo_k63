9b1ff8a597567daaed2581c2507a847804de93f6
binh.nguyenduc2000 <binh.nguyenduc2000@gmail.com>
2022-05-31 09:35:18 +0000

    Change Todo status

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -4,13 +4,29 @@
 　・チェックボックスにチェックが入っているか管理する
 　・チェックボックスにチェックが入っているかアイテムをグレーアウトする
 */
+import React, { useEffect, useState } from "react";
 function TodoItem({item}) {
+  const [style, setStyle] = useState("black");
+  const [isClicked, setClicked] = useState(false);
+  const handleClick = () => {
+    if (!isClicked){
+      setStyle("grey");
+      setClicked(true);
+    }
+    else {
+      setStyle("black");
+      setClicked(false);
+    }
+  };
+
+  
   return (
-    <label className="panel-block">
-      <input type="checkbox" />
+    <label className="panel-block" style={{color : style}}>
+      <input type="checkbox" onClick={handleClick} />
       {item.text}
     </label>
   );
 }
+
 
 export default TodoItem;
